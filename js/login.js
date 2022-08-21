1356f767a6dfe5d78bb3bf25bc35888e5a5ae3a8
Guillermo Morales <guillermo.gazille@gmail.com>
2022-08-21 02:49:44 -0300

    Actualizacion de Login con Google

--- js/login.js
+++ js/login.js
@@ -42,6 +42,28 @@ const login = () => {
   }
 };
 
+function parseJwt(token) {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+function handleCredentialResponse({ credential }) {
+  const Payload = parseJwt(credential);
+  localStorage.setItem("Email", Payload.email);
+  location.href = "index.html";
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("enviar").addEventListener("click", function () {
     login();
--- js/products.js
+++ js/products.js
@@ -4,10 +4,11 @@ const callApiCars = async () => {
   const resultado = await respuesta.json();
   const arrayAutos = resultado.products;
   document.getElementById("catName").innerHTML = resultado.catName;
-  arrayAutos.forEach((element) => {
-    document.getElementById(
-      "containerAutos"
-    ).innerHTML += ` <div class="container">
+  arrayAutos.forEach(
+    (element) =>
+      (document.getElementById(
+        "containerAutos"
+      ).innerHTML += ` <div class="container">
     <div class="list-group-item list-group-item-action cursor-active">
       <div class="row">
         <div class="col-3">
@@ -22,8 +23,8 @@ const callApiCars = async () => {
         </div>
       </div>
     </div>
-  </div> `;
-  });
+  </div> `)
+  );
 };
 
 callApiCars();
